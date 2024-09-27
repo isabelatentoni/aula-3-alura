9d1601c4fe2e00617a90bc3630f6644632161adf
isabelatentoni <isabela.tentoni.martins@gmail.com>
2024-09-26 22:33:03 -0300

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,38 +1,15 @@
-let cor;
-let circuloX; // horizontal
-let circuloY; // vertical
-
 function setup() {
-  createCanvas(400, 400); // width x height
-  background(color(100,0,0));
-  cor = color(random(0,255), random(0,255), random(0,255));
-  circuloX = [0,0,0];
-  circuloY = [random(height), random(height), random(height)];
+  createCanvas(600, 600);
+  background("black");
 }
 
-// circuloX = 0,0,0  
-// circuloY = 300, 150 , 150
-
-
-
-
 function draw() {
-  
-  fill(cor);
-  
-  //console.log (circuloX.length)
-  
-  for (let contador in circuloX) {
-    // console.log (contador)
-  circle(circuloX[contador], circuloY[contador], 50);  
-  circuloX[contador] += random (0,3);
-  circuloY[contador] += random (-3,3);
-    
-    if (circuloX[contador] >= width){
-      circuloX[contador] = 0;
-      circuloY[contador] = random(height);
-    }
-   
+  stroke("blue");
+  fill("pink");
+
+  // console.log(mouseIsPressed)
+
+  if (mouseIsPressed) {
+    rect(mouseX, mouseY, 50, 80);
   }
- 
 }
